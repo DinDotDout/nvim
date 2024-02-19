4915f32db038c336e9d98c173c68235c0439fc39
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-02-19 16:04:32 +0100

    Better text objects with mini.ai

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -196,7 +196,77 @@ return {
             wk.register(opts.defaults)
         end,
     },
+    -- Mostly from lazyvim
+    -- Mostly allows better actions jumping to them when found within n_lines
+    {
+        "echasnovski/mini.ai",
+        event = "VeryLazy",
+        opts = function()
+            local ai = require("mini.ai")
+            return {
+                n_lines = 500,
+                custom_textobjects = {
+                    -- Block
+                    o = ai.gen_spec.treesitter({
+                        a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                        i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+                    }, {}),
+                    -- Function
+                    f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+                    -- Class
+                    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+                    -- t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+                },
+            }
+        end,
+        config = function(_, opts)
+            require("mini.ai").setup(opts)
+            -- register all text objects with which-key
+            local function set_keymaps()
+                ---@type table<string, string|table>
+                local i = {
+                    [" "] = "Whitespace",
+                    ['"'] = 'Balanced "',
+                    ["'"] = "Balanced '",
+                    ["`"] = "Balanced `",
+                    ["("] = "Balanced (",
+                    [")"] = "Balanced ) including white-space",
+                    [">"] = "Balanced > including white-space",
+                    ["<lt>"] = "Balanced <",
+                    ["]"] = "Balanced ] including white-space",
+                    ["["] = "Balanced [",
+                    ["}"] = "Balanced } including white-space",
+                    ["{"] = "Balanced {",
+                    ["?"] = "User Prompt",
+                    _ = "Underscore",
+                    a = "Argument",
+                    b = "Balanced ), ], }",
+                    c = "Class",
+                    f = "Function",
+                    o = "Block, conditional, loop",
+                    q = "Quote `, \", '",
+                    t = "Tag",
+                }
+                local a = vim.deepcopy(i)
+                for k, v in pairs(a) do
+                    a[k] = v:gsub(" including.*", "")
+                end
 
+                local ic = vim.deepcopy(i)
+                local ac = vim.deepcopy(a)
+                for key, name in pairs({ n = "Next", l = "Last" }) do
+                    i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, ic)
+                    a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, ac)
+                end
+                require("which-key").register({
+                    mode = { "o", "x" },
+                    i = i,
+                    a = a,
+                })
+            end
+            set_keymaps()
+        end,
+    },
     --     {
     --   "folke/trouble.nvim",
     --   cmd = { "TroubleToggle", "Trouble" },
