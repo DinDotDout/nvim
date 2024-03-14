f37fa8f4386d8b281fc040285240d3653dd0fc02
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-03-14 15:50:39 +0100

    Ensure tab selection disabled

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -88,14 +88,17 @@ return {
                     max_width = 20,
                 },
                 mapping = cmp.mapping.preset.insert({
+                    ["<tab>"] = cmp.config.disable,
+                    ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
                     ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
+
                     ["<C-e>"] = cmp.mapping.abort(),
                     -- ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                    ["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                     ["<S-CR>"] = cmp.mapping.confirm({
                         behavior = cmp.ConfirmBehavior.Replace,
                         select = true,
