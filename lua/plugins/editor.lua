9e69030668ea796d8f574d3b321113c6340aa1b5
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-04-04 12:17:13 +0200

    toggle term for quick builds

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,52 @@
 return {
+    {
+        "akinsho/toggleterm.nvim",
+        config = function()
+            require("toggleterm").setup({
+                open_mapping = [[<c-\>]],
+                hide_numbers = true, -- hide the number column in toggleterm buffers
+                shade_filetypes = {},
+                -- autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+                shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+                -- shading_factor = '<number>', -- the percentage by which to lighten terminal background, default: -30 (gets multiplied by -3 if background is light)
+                start_in_insert = true,
+                insert_mappings = true, -- whether or not the open mapping applies in insert mode
+
+                terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+                -- persist_size = true,
+                -- persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+                -- direction = 'vertical' | 'horizontal' | 'tab' | 'float',
+                -- use vim copy mode
+                direction = "float",
+                -- direction = "tab",
+                close_on_exit = true, -- close the terminal window when the process exits
+
+                -- Change the default shell. Can be a string or a function returning a string
+                shell = vim.o.shell,
+                auto_scroll = true, -- automatically scroll to the bottom on terminal output
+                -- This field is only relevant if direction is set to 'float'
+                float_opts = {
+                    border = "curved",
+                    -- title_pos = 'left' | 'center' | 'right', position of the title of the floating window
+                },
+                -- winbar = {
+                -- enabled = false,
+                -- name_formatter = function(term) --  term: Terminal
+                -- return term.name
+                -- end
+                -- },
+                vim.keymap.set("t", "<esc>", [[<C-\><C-n>]]),
+                vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]]),
+                vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]]),
+                vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]]),
+                vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]]),
+                vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]]),
+                vim.keymap.set("n", "<leader>cx", '<cmd>TermExec cmd="./build.sh"<cr>', {desc = "Toggleterm build.sh"}),
+                --   -- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+            })
+        end,
+    },
+
     { "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" }, opts = {} },
     { -- Smart comments
         "numToStr/Comment.nvim",
@@ -7,9 +55,13 @@ return {
         config = function()
             require("Comment").setup()
             vim.keymap.set("n", "<C-_>", "gcc", { remap = true }, { desc = "Comment Line" }) -- Needed for tmux
-            vim.keymap.set("x", "<C-_>", "gb", { remap = true }, { desc = "Comment Block" }) -- Needed for tmux
+            vim.keymap.set("x", "<C-_>", "gc", { remap = true }, { desc = "Comment Line" }) -- Needed for tmux
+
             vim.keymap.set("n", "<C-/>", "gcc", { remap = true }, { desc = "Comment Line" })
-            vim.keymap.set("x", "<C-/>", "gb", { remap = true }, { desc = "Comment Block" })
+            vim.keymap.set("x", "<C-/>", "gc", { remap = true }, { desc = "Comment Line" })
+
+            vim.keymap.set("n", "gC", "gcc", { remap = true }, { desc = "Comment Line" })
+            vim.keymap.set("x", "gC", "gc", { remap = true }, { desc = "Comment Line" })
         end,
     },
 
@@ -44,18 +96,18 @@ return {
                 harpoon:list():select(4)
             end, { desc = "Jump to File 4 (Harpoon)" })
 
-            -- keymap.set("n", "<C-z>", function()
-            --     harpoon:list():select(1)
-            -- end, { desc = "Jump to File 1 (Harpoon)" })
-            -- keymap.set("n", "<C-x>", function()
-            --     harpoon:list():select(2)
-            -- end, { desc = "Jump to File 2 (Harpoon)" })
-            -- keymap.set("n", "<C-c>", function()
-            --     harpoon:list():select(3)
-            -- end, { desc = "Jump to File 3 (Harpoon)" })
-            -- keymap.set("n", "<C-v>", function()
-            --     harpoon:list():select(4)
-            -- end, { desc = "Jump to File 4 (Harpoon)" })
+            keymap.set("n", "<C-z>", function()
+                harpoon:list():select(1)
+            end, { desc = "Jump to File 1 (Harpoon)" })
+            keymap.set("n", "<C-x>", function()
+                harpoon:list():select(2)
+            end, { desc = "Jump to File 2 (Harpoon)" })
+            keymap.set("n", "<C-c>", function()
+                harpoon:list():select(3)
+            end, { desc = "Jump to File 3 (Harpoon)" })
+            keymap.set("n", "<C-v>", function()
+                harpoon:list():select(4)
+            end, { desc = "Jump to File 4 (Harpoon)" })
         end,
     },
 
@@ -254,6 +306,7 @@ return {
             wk.register(opts.defaults)
         end,
     },
+
     -- Mostly from lazyvim
     -- Allows better actions jumping to them when found within n_lines
     {
@@ -273,7 +326,8 @@ return {
                     f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
                     -- Class
                     c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-                    -- t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+                    -- a = ai.gen_spec.treesitter({ a = "@parameter.outer", i = "@parameter.inner" }, {}),
+                    t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
                 },
             }
         end,
@@ -316,11 +370,11 @@ return {
                     i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, ic)
                     a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, ac)
                 end
-                require("which-key").register({
-                    mode = { "o", "x" },
-                    i = i,
-                    a = a,
-                })
+                --[[ require("which-key").register({
+        mode = { "o", "x" },
+        i = i,
+        a = a,
+        }) ]]
             end
             set_keymaps()
         end,
