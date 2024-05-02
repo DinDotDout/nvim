cae20f7b0c2027be3b112f827a4768b958778446
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-05-02 11:12:59 +0200

    Updated copilot chat version/branch.

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -18,23 +18,63 @@ return {
     -- },
     {
         "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
         opts = {
-            show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
             debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
-            disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
+            disable_extra_info = "yes", -- Disable extra information (e.g: system prompt) in the response.
+            window = { layout = "float" },
             -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
         },
-        build = function()
-            vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
-        end,
+        dependencies = {
+            { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+            { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+        },
+        -- build = function()
+        --     vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+        -- end,
         event = "VeryLazy",
         keys = {
             {
-                "<leader>cc",
-                ":CopilotChatInPlace<cr>",
-                mode = "x",
+                "<leader>ct",
+                ":CopilotChatToggle<cr>",
+                mode = {"n"},
                 desc = "CopilotChat - Run in-place code",
             },
+            -- lazy.nvim keys
+
+            -- TODO: Add prompt for search grep in Telescope
+            -- Quick chat with Copilot
+            {
+                "<leader>cc",
+                mode = {"n", "v", "x"},
+                function()
+                    local input = vim.fn.input("Quick Chat: ")
+                    if input ~= "" then
+                        require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+                    end
+                end,
+                desc = "CopilotChat - Quick chat",
+            },
+            -- lazy.nvim keys
+
+            -- Show help actions with telescope
+            -- {
+            --     "<leader>cch",
+            --     function()
+            --         local actions = require("CopilotChat.actions")
+            --         require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+            --     end,
+            --     desc = "CopilotChat - Help actions",
+            -- },
+            -- -- Show prompts actions with telescope
+            -- {
+            --     "<leader>ccp",
+            --     function()
+            --         local actions = require("CopilotChat.actions")
+            --         require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+            --     end,
+            --     desc = "CopilotChat - Prompt actions",
+            -- },
         },
     },
 }
