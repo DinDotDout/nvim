6a79e5b5c4724a2f1edec8f63c27602fd09404fc
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-05-02 13:26:23 +0200

    refactor(lsp): Reindent

--- lua/plugins/lsp/format.lua
+++ lua/plugins/lsp/format.lua
@@ -1,39 +1,38 @@
--- return {}
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        json = { { "prettierd", "prettier" } },
-        markdown = { { "prettierd", "prettier" } },
-        rust = { "rustfmt" },
-        yaml = { "yamlfix" },
-        toml = { "taplo" },
-        css = { { "prettierd", "prettier" } },
-        scss = { { "prettierd", "prettier" } },
-        bash = { "shfmt" },
-        gdscript = { "gdformat" },
-        cmake = { "cmakelang" },
-        cpp = { "clang-format" },
-      },
-    })
+        conform.setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                json = { { "prettierd", "prettier" } },
+                markdown = { { "prettierd", "prettier" } },
+                rust = { "rustfmt" },
+                yaml = { "yamlfix" },
+                toml = { "taplo" },
+                css = { { "prettierd", "prettier" } },
+                scss = { { "prettierd", "prettier" } },
+                bash = { "shfmt" },
+                gdscript = { "gdformat" },
+                cmake = { "cmakelang" },
+                cpp = { "clang-format" },
+            },
+        })
 
-    format_on_save = {
-      lsp_fallback = true,
-      async = false,
-      timout_ms = 500,
-    }
+        format_on_save = {
+            lsp_fallback = true,
+            async = false,
+            timout_ms = 500,
+        }
 
-    vim.keymap.set({ "n", "v" }, "<leader>cf", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
-  end,
+        vim.keymap.set({ "n", "v", "x" }, "<leader>cf", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
+    end,
 }
