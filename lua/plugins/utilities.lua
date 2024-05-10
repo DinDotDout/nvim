1222ec576c6eefd71ccb3dc08d224ff15810b986
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-05-10 11:18:13 +0200

    gx goes to link fix due to oil nvim replacing fe.

--- lua/plugins/utilities.lua
+++ lua/plugins/utilities.lua
@@ -78,4 +78,22 @@ return {
         },
         main = "ibl",
     },
+
+    {
+        "chrishrb/gx.nvim",
+        keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+        cmd = { "Browse" },
+        init = function()
+            vim.g.netrw_nogx = 1 -- disable netrw gx
+        end,
+        dependencies = { "nvim-lua/plenary.nvim" },
+        submodules = false,
+        config = function()
+            require("gx").setup({
+                handler_options = {
+                    search_engine = "duckduckgo",
+                },
+            })
+        end,
+    },
 }
