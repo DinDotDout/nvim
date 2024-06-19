bb8026f2f66ddb2cfac38fc8bdf12713e23fd89e
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-06-19 15:59:25 +0200

    Moved more things into "c" keybind space

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -76,7 +76,7 @@ local function keymaps(event)
     map("gt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype definition")
 
     map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-    map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+    map("<leader>cS", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
     map("K", vim.lsp.buf.hover, "Hover Documentation")
 
     map("<C-S-k>", vim.lsp.buf.signature_help, "Signature help") -- show
