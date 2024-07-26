596313a7a72411eafdd908c0d3c00f8d367417ae
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-07-26 09:09:36 +0200

    Changed dap keymaps

--- lua/plugins/lsp/dap.lua
+++ lua/plugins/lsp/dap.lua
@@ -23,110 +23,99 @@ return {
         },
     },
     {
-        "rcarriga/nvim-dap-ui",
-        dependencies = "nvim-neotest/nvim-nio",
-        opts = {
-            icons = {
-                expanded = "󰅀",
-                collapsed = "󰅂",
-                current_frame = "󰅂",
-            },
-            -- layouts = {
-            --     {
-            --         elements = { "console", "watches" },
-            --         position = "bottom",
-            --         size = 15,
-            --     },
-            -- },
-            -- expand_lines = false,
-            -- controls = {
-            --     enabled = false,
-            -- },
-            -- floating = {
-            --     border = "rounded",
-            -- },
-            -- render = {
-            --     indent = 2,
-            -- Hide variable types as C++'s are verbose
-            -- max_type_length = 0,
-            -- },
-        },
-        keys = {
-            {
-                "<F1>",
-                function()
-                    require("dapui").toggle({ reset = true })
-                end,
-                desc = "Toggle DAP UI",
-            },
-            {
-                "<F14>",
-                function()
-                    require("dapui").eval()
-                end,
-                desc = "DAP Eval",
-            },
-        },
-    },
-    {
         "mfussenegger/nvim-dap",
         dependencies = {
             -- Runs preLaunchTask / postDebugTask if present
             { "stevearc/overseer.nvim", config = true },
             "williamboman/mason.nvim",
+            "nvim-neotest/nvim-nio",
+            {
+                "rcarriga/nvim-dap-ui",
+                opts = {
+                    icons = {
+                        expanded = "󰅀",
+                        collapsed = "󰅂",
+                        current_frame = "󰅂",
+                    },
+                    -- layouts = {
+                    --     {
+                    --         elements = { "console", "watches" },
+                    --         position = "bottom",
+                    --         size = 15,
+                    --     },
+                    -- },
+                    -- expand_lines = false,
+                    -- controls = {
+                    --     enabled = false,
+                    -- },
+                    -- floating = {
+                    --     border = "rounded",
+                    -- },
+                    -- render = {
+                    --     indent = 2,
+                    -- Hide variable types as C++'s are verbose
+                    -- max_type_length = 0,
+                    -- },
+                },
+                keys = {
+                    {
+                        "<leader>tu",
+                        function()
+                            require("dapui").toggle({ reset = true })
+                        end,
+                        desc = "Toggle DAP UI",
+                    },
+                    {
+                        "<leader>?",
+                        function()
+                            require("dapui").eval(nil, { enter = true })
+                        end,
+                        desc = "DAP Eval",
+                    },
+                },
+            },
         },
         keys = {
             {
-                "<leader>ds",
+                "<leader>ts",
                 function()
                     local widgets = require("dap.ui.widgets")
                     widgets.centered_float(widgets.scopes, { border = "rounded" })
                 end,
                 desc = "DAP Scopes",
             },
-            {
-                "<F2>",
-                function()
-                    require("dap.ui.widgets").hover(nil, { border = "rounded" })
-                end,
-            },
+            -- {
+            --     "<F2>",
+            --     function()
+            --         require("dap.ui.widgets").hover(nil, { border = "rounded" })
+            --     end,
+            -- },
             { "<F4>", "<CMD>DapTerminate<CR>", desc = "DAP Terminate" },
             {
                 "<F5>",
                 function()
                     require("dap").continue()
                 end,
-                -- "<CMD>DapContinue<CR>",
                 desc = "DAP Continue",
             },
             {
-                "<F17>",
+                "<F6>",
                 function()
                     require("dap").run_last()
                 end,
                 desc = "Run Last",
             },
             {
-                "<F6>",
+                "<F7>",
                 function()
                     require("dap").run_to_cursor()
                 end,
                 desc = "Run to Cursor",
             },
-            -- { "<F9>", "<CMD>DapToggleBreakpoint<CR>", desc = "Toggle Breakpoint" },
             { "<leader>t", "<CMD>DapToggleBreakpoint<CR>", desc = "Toggle Breakpoint" },
-            {
-                "<F21>",
-                function()
-                    vim.ui.input({ prompt = "Breakpoint condition: " }, function(input)
-                        require("dap").set_breakpoint(input)
-                    end)
-                end,
-                desc = "Conditional Breakpoint",
-            },
-            { "<F10>", "<CMD>DapStepOver<CR>", desc = "Step Over" },
-            { "<F11>", "<CMD>DapStepInto<CR>", desc = "Step Into" },
-            { "<F12>", "<CMD>DapStepOut<CR>", desc = "Step Out" },
+            { "<F1>", "<CMD>DapStepOver<CR>", desc = "Step Over" },
+            { "<F2>", "<CMD>DapStepInto<CR>", desc = "Step Into" },
+            { "<F3>", "<CMD>DapStepOut<CR>", desc = "Step Out" },
         },
         config = function()
             -- It appears that codelldb works as cpp tools for now, keep cpptools?
