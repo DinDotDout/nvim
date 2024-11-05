d789e06c2c3e73b0bc4cd81be9049163c2d021e1
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-11-05 20:22:03 +0100

    remoded toggle term

--- lua/plugins/code_edit.lua
+++ lua/plugins/code_edit.lua
@@ -1,85 +1,10 @@
 return {
-    {
-        "akinsho/toggleterm.nvim",
-        config = function()
-            require("toggleterm").setup({
-                open_mapping = [[<c-\>]],
-                hide_numbers = true, -- hide the number column in toggleterm buffers
-                shade_filetypes = {},
-                -- autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
-                shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
-                -- shading_factor = '<number>', -- the percentage by which to lighten terminal background, default: -30 (gets multiplied by -3 if background is light)
-                start_in_insert = true,
-                insert_mappings = true, -- whether or not the open mapping applies in insert mode
-
-                terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
-                -- persist_size = true,
-                -- persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
-                -- direction = 'vertical' | 'horizontal' | 'tab' | 'float',
-                -- use vim copy mode
-                direction = "float",
-                -- direction = "tab",
-                close_on_exit = true, -- close the terminal window when the process exits
-
-                -- Change the default shell. Can be a string or a function returning a string
-                shell = vim.o.shell,
-                auto_scroll = true, -- automatically scroll to the bottom on terminal output
-                -- This field is only relevant if direction is set to 'float'
-                float_opts = {
-                    border = "curved",
-                    -- title_pos = 'left' | 'center' | 'right', position of the title of the floating window
-                },
-                -- winbar = {
-                -- enabled = false,
-                -- name_formatter = function(term) --  term: Terminal
-                -- return term.name
-                -- end
-                -- },
-                vim.keymap.set("t", "<esc>", [[<C-\><C-n>]]),
-                vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]]),
-                vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]]),
-                vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]]),
-                vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]]),
-                vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]]),
-                vim.keymap.set(
-                    "n",
-                    "<leader>ct",
-                    '<cmd>TermExec cmd="./build/build_run.sh Release"<cr>',
-                    { desc = "Toggleterm build.sh" }
-                ),
-                vim.keymap.set(
-                    "n",
-                    "<leader>cx",
-                    '<cmd>TermExec cmd="./build/build_run.sh Debug"<cr>',
-                    { desc = "Toggleterm build.sh" }
-                ),
-                --   -- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-            })
-        end,
-    },
-
     { "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" }, opts = {} },
-    { -- Smart comments
-        "numToStr/Comment.nvim",
-        -- event = { "BufRead", "BufNewFile", "BufWritePost" },
-        lazy = false,
-        config = function()
-            require("Comment").setup()
-            vim.keymap.set("n", "<C-_>", "gcc", { remap = true }, { desc = "Comment Line" }) -- Needed for tmux
-            vim.keymap.set("x", "<C-_>", "gc", { remap = true }, { desc = "Comment Line" }) -- Needed for tmux
-            vim.keymap.set("n", "<C-/>", "gcc", { remap = true }, { desc = "Comment Line" })
-            vim.keymap.set("x", "<C-/>", "gc", { remap = true }, { desc = "Comment Line" })
-            vim.keymap.set("n", "gC", "gcc", { remap = true }, { desc = "Comment Line" })
-            vim.keymap.set("x", "gC", "gc", { remap = true }, { desc = "Comment Line" })
-        end,
-    },
-
     {
         "mbbill/undotree",
         enabled = true,
         keys = { { "<leader>ut", "<cmd>UndotreeToggle<CR>", desc = "Undotree" } },
     },
-
 
     -- Mostly from lazyvim
     -- Allows better actions jumping to them when found within n_lines
